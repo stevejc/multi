2aae579ffb34819e64bab80f1a296908534b6504
Steve <stevemargc@gmail.com>
2014-07-20 18:40:44 -0400

    added form builder and updated accounts edit page to use the new form builder

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Multi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W( #{config.root}/lib )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -19,5 +20,6 @@ module Multi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_view.field_error_proc = Proc.new {|html, instance| html}
   end
 end
